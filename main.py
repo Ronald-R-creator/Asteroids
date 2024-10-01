d7dd27a6bdf4c73856dacd7d34ac33637a4a2584
ronald <ronny767676@gmail.com>
2024-10-01 17:36:54 -0500

    Circleshape file added

--- main.py
+++ main.py
@@ -11,12 +11,16 @@ print(f"Screen height: {SCREEN_HEIGHT}")
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()    
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
         pygame.display.flip()
+        clock.tick(60)
+        dt = clock.tick(60) / 1000.0
 
 if __name__ == "__main__":
     main()
