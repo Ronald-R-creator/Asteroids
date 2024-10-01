4b32c904b78ac3262fab64227a79593137997947
ronald <ronny767676@gmail.com>
2024-10-01 16:20:35 -0500

    Loop Test

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 import pygame
+from constants import *
 def game_start():
     print("Starting asteroids!")
 
 
-
-
-from constants import *
 game_start()
-
 print(f"Screen width: {SCREEN_WIDTH}")
-
 print(f"Screen height: {SCREEN_HEIGHT}")
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+while True:
+    pygame.Surface.fill(black)
+    pygame.display.flip()
 
 
 
 if __name__ == "__main__":
+    pygame.init()
     main()
