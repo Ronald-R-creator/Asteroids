0d149a1b203f3c039c01c10ac65ae08fe7536029
ronald <ronny767676@gmail.com>
2024-10-01 16:19:05 -0500

    First Commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import pygame
+def game_start():
+    print("Starting asteroids!")
+
+
+
+
+from constants import *
+game_start()
+
+print(f"Screen width: {SCREEN_WIDTH}")
+
+print(f"Screen height: {SCREEN_HEIGHT}")
+
+
+
+if __name__ == "__main__":
+    main()
